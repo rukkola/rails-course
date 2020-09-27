62d749aa14d028058df32eee974195366091ef7c
Serg <seregaproger@yandex.ru>
2020-09-27 19:11:46 +0300

    add callback

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :desc, presence: true
+
+  after_initialize { p 'init'}
+  after_save       { p 'save' }
+  after_create     { p 'create' }
+  after_update     { p 'update' }
+  after_destroy    { p 'destroy' }
 end
