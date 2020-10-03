969269add54c882ee1e97834b6f5493304458705
Serg <seregaproger@yandex.ru>
2020-10-03 22:58:38 +0300

    i18n

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module RailsCourse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ru
   end
 end
