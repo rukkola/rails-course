5356291f87f76b8ab105220aa73648c6e61e27fc
Serg <seregaproger@yandex.ru>
2020-10-07 22:23:11 +0300

    gem devise

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RailsCourse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = :ru
+    #config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
   end
 end
